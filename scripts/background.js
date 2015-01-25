60a29baa0b192994fa0eec7c879ce887926fe19c
ljgstudy <ljgstudy@163.com>
2015-01-25 22:07:15 +0800

    code refecoring

--- scripts/background.js
+++ scripts/background.js
@@ -1,45 +1,59 @@
+// Request listener.
+// Mainly to handle the request from content page
+// and return the translated data.
 chrome.extension.onRequest.addListener(
-  function(request, sender, sendResponse) {
-    if(request != null && $.trim(request.text) != null){
-    	var resultData = translateWithYouDao(request.text);
-    	sendResponse({text: resultData});
-    }else{
-    	sendResponse({text: 'error'});
+    function (request, sender, sendResponse) {
+        
+        if (request != null && $.trim(request.text) != null) {
+
+            var translater = new Translater();
+            
+            // Gets data and return to caller.
+            translater.translateWithYouDao(request.text)
+                .then(function (result) {
+                    sendResponse({ text: result });
+                });
+        }
+    });
+
+// Gets translated data from server.
+var Translater = function () {
+    
+    // Service method.
+    // Gets translated text from YouDao open API.
+    this.translateWithYouDao = function (text) {
+
+        var type = 'get';
+        var url  = 'http://fanyi.youdao.com/openapi.do';
+        var data = {
+            keyfrom : 'notebook',
+            key     : '1581556187',
+            type    : 'data',
+            doctype : 'json',
+            version : '1.1',
+            q       :  text
+        };
+
+        return getData(type, url, data);
+    };
+
+    // Comment method to get data with ajax from server.
+    var getData = function (type, url, data) {
+
+        var dtd = $.Deferred();
+
+        $.ajax({
+            type    : type,
+            url     : url,
+            data    : data,
+            success : function (result) {
+                dtd.resolve(result);
+            },
+            error: function (result) {
+                dtd.resolve('timeout or errors in server');
+            }
+        });
+
+        return dtd.promise();
     }
-});
-
-function translateWithYouDao(text){
-	var type = 'get';
-	var url = 'http://fanyi.youdao.com/openapi.do';
-	var data = {
-			keyfrom: 'notebook',
-			key: '1581556187',
-			type: 'data',
-			doctype: 'json',
-			version: '1.1',
-			q: text
-	};
-	return getData(type, url, data);
-}
-
-//Sends request for specify API
-//type: requst type('post' or 'get')
-//url: address of API
-//data: specify parameter to invoke API
-function getData(type, url, data){
-	var resultData = '';
-	$.ajax({
-		type: type,
-		url: url,
-		data: data,
-		async: false,
-		success: function(result){
-			resultData = result;
-		},
-		error: function(result){
-			resultData = result;
-		}
-	});
-	console.log(resultData);
-	return resultData;
 }
--- scripts/translate.js
+++ scripts/translate.js
@@ -1,131 +1,157 @@
-var xx;
-var yy;
-var TimeFn = null;
+/*
+Copyright (C) 2014 Byron Li
 
-// Injects the window element used to show translate content.
-//$(document).ready(function(){
-//	if($('#popup_byron').length <= 0){
-//		var divElement = generatePopulateWindow();
-//		$('body').append(divElement);
-//	}
-//});
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
 
-// Record current position of mouse.
-// And mainly uses this position to show translated result.
-$(document).mousemove(function(e){
-	// Gets the selected text position.
-	xx = e.originalEvent.x || e.originalEvent.layerX || 0; 
-	yy = e.originalEvent.y || e.originalEvent.layerY || 0;
-});
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 
-// Mouse click event listener.
-// Decides when to show the translated result
-// and when to hide it.
-$(document).click(function(){
-    clearTimeout(TimeFn);
-    TimeFn = setTimeout(function(){
-    	var selectedText = $.trim(window.getSelection().toString());
-        if(selectedText != null 
-        	&& selectedText.length > 0
-        	&& isEnglish(selectedText)){
-        	translateAndShowData(xx, yy, showData);
-        }else{
-        	$('#popup_byron').hide('slow');
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+(function () {
+    
+    'use strict';
+    
+    var TimeFn;
+
+    // Binds click event.
+    $(document).click(function (e) {
+
+        // Cancels last timeout settings.
+        clearTimeout(TimeFn);
+
+        var position = EventPositionHelper.getMouseEventPosition(e);
+
+        TimeFn = setTimeout(function () {
+            var selectedText = $.trim(window.getSelection().toString());
+            if (selectedText != null && selectedText.length > 0 && Translater.isEnglish(selectedText)) {
+                Translater
+                    .translateWithWait(selectedText)
+                    .then(function (result) {
+                        Translater.showResult(position.xx, position.yy, result.text || 'No Data!');
+                    })
+            } else {
+                $('#_popup_').hide('slow');
+            }
+        }, 300);
+    });
+
+    // Binds double click event.
+    $(document).dblclick(function (e) {
+
+        // Cancels last timeout settings
+        clearTimeout(TimeFn);
+
+        var position = EventPositionHelper.getMouseEventPosition(e);
+        var selectedText = $.trim(window.getSelection().toString());
+
+        if (selectedText != null && selectedText.length > 0 && Translater.isEnglish(selectedText)) {
+            Translater
+                .translateWithWait(selectedText)
+                .then(function (result) {
+                    Translater.showResult(position.xx, position.yy, result.text || 'No Data!')
+                });
         }
-    },300);
-});
+    });
 
-// Mouse double click event.
-// Decides when to show the translated result.
-$(document).dblclick(function(){
-    clearTimeout(TimeFn);
-    var selectedText = $.trim(window.getSelection().toString());
-    if(selectedText != null 
-    	&& selectedText.length > 0
-    	&& isEnglish(selectedText)){
-    	translateAndShowData(xx, yy, showData);
-    }else{
-    	// Nothing to do here!
+    // Mainly to get translated text and show to user.
+    var Translater = {
+        
+        translateWithWait: function (selectedText) {
+
+            var dtd = $.Deferred();
+
+            if (selectedText != null && selectedText.length > 0) {
+                chrome.extension.sendRequest({
+                        text: selectedText
+                    },
+                    function (response) {
+                        dtd.resolve(response);
+                    });
+            }
+
+            return dtd.promise();
+        },
+
+        showResult: function (xx, yy, data) {
+            var defaultResultWindow =
+                "<div id='_popup_' style='width:280px; border: 1px solid #f27405; background-color: white;                          position: absolute; display: none; text-align: left'>\
+                    <div id='_title_' style='background:#f27405; height: 30px'></div>\
+                    <div id='_content_'>\
+                        <b id='_result_' style='font:bold 25px arial,sans-serif; padding: 5px 12px; word-wrap:                                break-word;'>No Result</b><br />\
+                        <hr style='border:0;background-color:#f27405;height:1px' />\
+                        <ul id='_webresult_' style='list-style:none; font:12px arial,sans-serif; padding: 2px                                 12px'>\
+                        </ul>\
+                    </div>\
+                </div>";
+
+            // Injects the window element which is used to show translated result.
+            if ($('#_popup_').length <= 0) {
+                
+                //var divElement = generatePopulateWindow();
+                $('body').append(defaultResultWindow);
+            }
+            
+            // Clear previous data.
+            $('#_webresult_').empty();
+
+            // Sets the position of result window.
+            $('#_popup_').css('left', xx);
+            $('#_popup_').css('top', yy);
+
+            // Fills data into result window.
+            if (data != null) {
+
+                // Traditional translated result
+                if (data.translation != null && data.translation.length > 0) {
+                    $('#_result_').text(data.translation[0]);
+                }
+
+                // Results from web.
+                if (data.web != null && data.web.length > 0) {
+                    for (var i = 0; i < data.web.length; i++) {
+                        var eachWebResult = $('<li></li>')
+                        var eachResultText = data.web[i].key;
+                        var eachResultValue = '';
+                        for (var j = 0; j < data.web[i].value.length; j++) {
+                            eachResultValue += data.web[i].value[j] + '; ';
+                        }
+                        eachWebResult.text(eachResultText + ': ' + eachResultValue);
+                        $('#_webresult_').append(eachWebResult);
+                    }
+                }
+            }
+
+            $('#_popup_').show('slow');
+        },
+
+        // Checks whether current selected text is English letter.
+        isEnglish: function (text) {
+            var parent = /^[A-Za-z]+$/;
+
+            if (parent.test(text)) {
+                return true;
+            } else {
+                return false;
+            }
+        }
     }
-})
 
-// Requests the translate API and gets the translated result.
-function translateAndShowData(xx, yy, showData){
-	var selectedText = $.trim(window.getSelection().toString());
-	if(selectedText != null && selectedText.length > 0){
-			// lastTime = Math.round(new Date().getTime());
-			chrome.extension.sendRequest({text: selectedText}, function(response){
-			// Append element to DOM
-			showData(xx, yy, response.text);
-		});
-	}
-}
+    // Mainly to compute the position of translated result window.
+    var EventPositionHelper = {
+        getMouseEventPosition: function (e) {
+            var _xx = e.originalEvent.x || e.originalEvent.layerX || 0;
+            var _yy = e.originalEvent.y || e.originalEvent.layerY || 0;
+            
+            // Moves shown result window down a little,
+            // which avoise covering the original page words.
+            var kdheight = $(document).scrollTop();
+            var actualYY = _yy + kdheight + 10; // Scrren Height + Scroll Height + Offset
 
-// Call back function.
-// Appends the translated result to window
-// and show this window.
-function showData(xx, yy, data){
-	// Injects the window element which is used to show translated result.
-	if($('#popup_byron').length <= 0){
-		var divElement = generatePopulateWindow();
-		$('body').append(divElement);
-	}
-	// Clear previous data.
-	$('#webresult').empty();
-
-	// Sets the position of result window.
-	var kdheight =  $(document).scrollTop();
-    $('#popup_byron').css('top', yy + kdheight + 10);
-	$('#popup_byron').css('left', xx);
-
-	// Fills data into result window.
-	if(data != null){
-
-		// Traditional translated result
-		if(data.translation != null && data.translation.length > 0){
-			$('#result').text(data.translation[0]);
-		}
-
-		// Results from web.
-		if(data.web != null && data.web.length > 0){
-			for(var i = 0; i < data.web.length; i++){
-				var eachWebResult = $('<li></li>')
-				var eachResultText = data.web[i].key;
-				var eachResultValue = '';
-				for(var j = 0; j < data.web[i].value.length; j++){
-					eachResultValue += data.web[i].value[j] + '; ';
-				}
-				eachWebResult.text(eachResultText + ': ' + eachResultValue);
-				$('#webresult').append(eachWebResult);
-			}
-		}
-
-	}
-
-	$('#popup_byron').show('slow');
-}
-
-// Window elements used to display the translated result.
-function generatePopulateWindow(){
-	var popupWindow = 
-		"<div id='popup_byron' style='width:280px; border: 1px solid #f27405; background-color: white; position: absolute; display: none'>\
-			<div id='title' style='background:#f27405; height: 30px'></div>\
-			<div id='content'>\
-				<b id='result' style='font:bold 25px arial,sans-serif; padding: 5px 12px; word-wrap: break-word;'>No Result</b><br />\
-				<hr style='border:0;background-color:#f27405;height:1px' />\
-				<ul id='webresult' style='list-style:none; font:12px arial,sans-serif; padding: 2px 12px'>\
-				</ul>\
-			</div>\
-		</div>";
-	return popupWindow;
-}
-
-// Check whether the all selected text is English letter.
-function isEnglish(text){
-	var parent=/^[A-Za-z]+$/;
-	if(parent.test(text)){
-	  	return true;
-	}else{
-	  return false;
-  	} 	
-}
+            return {
+                xx: _xx,
+                yy: actualYY
+            }
+        }
+    }
+}());
